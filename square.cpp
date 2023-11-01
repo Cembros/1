83af45f3f0cfa44bd49d533883ea14b894fbb55c
Cembros <147919305+Cembros@users.noreply.github.com>
2023-11-01 21:53:22 +0100

    Update square.cpp

--- square.cpp
+++ square.cpp
@@ -1,20 +1,23 @@
 #include <iostream>
 using namespace std;
 int main() {
-    int size;
     char symbol;
-    cout << "Enter the number of character: ";
-    cin >> size;
-    if (size <=0){
-        cout<<"Invalid value entered" ;
-        return 1;
-    }
-    cout << "Enter a character: ";
-    cin >> symbol;
-    for(int i = 0; i < size; i++){
-        for(int h = 0; h < size; h++){
-            cout << symbol << ' ';
+    while (true) {
+        int size;
+        cout << "Enter the number of character: ";
+        cin >> size;
+        if (size <= 0) {
+            cout << "Invalid value entered  " << endl;
+        } else {
+            cout << "Enter a character: ";
+            cin >> symbol;
+            for (int i = 0; i < size; i++) {
+                for (int h = 0; h < size; h++) {
+                    cout << symbol << ' ';
+                }
+                cout << endl;
+            }
+            break;
         }
-        cout << endl;
     }
 }
