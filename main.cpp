e6dc31e7e6be8cde00c38f6a510915e5d6d18ee1
Cembros <Zeymon100@gmail.com>
2023-12-02 11:10:43 +0100

    create a banking system that allows you to register and log in, view the balance and more, all data is stored in the database

--- main.cpp
+++ main.cpp
@@ -1,17 +1,187 @@
 #include <iostream>
+#include <string>
+#include <vector>
+
 using namespace std;
-int main() {
-    int size;
-    char symbol;
-    cout << "Enter a character: ";
-    cin >> symbol;
-    cout << "Enter the number of character: ";
-    cin >> size;
-    for(int i = 0; i < size; i++){
-        for(int h = 0; h < size; h++){
-            cout << symbol << ' ';
+
+struct User {
+    int id;
+    string login;
+    string password;
+    bool psswrdHasCaplet;
+    bool psswrdHasSmllet;
+    double balance;
+    string registerDate;
+};
+
+vector<User> database;
+
+bool isUserExists(const string& login, const string& password) {
+    for (const User& user : database) {
+        if (user.login == login && user.password == password) {
+            return true;
         }
-        cout << endl;
+    }
+    return false;
+}
+
+int getUserIndex(const string& login, const string& password) {
+    for (size_t i = 0; i < database.size(); ++i) {
+        if (database[i].login == login && database[i].password == password) {
+            return static_cast<int>(i);
+        }
+    }
+    return -1;
+}
+
+void showBalance(const User& user) {
+    cout << "Balance for user " << user.login << ": $" << user.balance << endl;
+}
+void withdrawCash(User& user, double amount) {
+    if (amount > 0) {
+        if (amount <= user.balance) {
+            user.balance -= amount;
+            cout << "Withdrawal successful. Remaining balance: $" << user.balance << endl;
+        } else {
+            cout << "Error: Insufficient balance. You cannot withdraw more than your current balance." << endl;
+        }
+    } else {
+        cout << "Error: Invalid withdrawal amount." << endl;
     }
 }
 
+void depositCash(User& user, double amount) {
+    if (amount > 0) {
+        user.balance += amount;
+        cout << "Deposit successful. You added $" << amount << " to your balance. New balance: $" << user.balance << endl;
+    } else {
+        cout << "Invalid deposit amount." << endl;
+    }
+}
+
+void registerUser() {
+    User newUser;
+    cout << "Enter login: ";
+    cin >> newUser.login;
+
+    while (true) {
+        cout << "Enter password: ";
+        cin >> newUser.password;
+
+        if (newUser.password.length() < 6) {
+            cout << "Error: Password must be at least 6 characters long." << endl;
+            continue;
+        }
+
+        int letterCount = 0;
+        newUser.psswrdHasCaplet = false;
+        newUser.psswrdHasSmllet = false;
+
+        for (char ch : newUser.password) {
+            if (isalpha(ch)) {
+                letterCount++;
+                if (isupper(ch)) {
+                    newUser.psswrdHasCaplet = true;
+                } else if (islower(ch)) {
+                    newUser.psswrdHasSmllet = true;
+                }
+            }
+        }
+
+        if (letterCount < 2 || !newUser.psswrdHasCaplet || !newUser.psswrdHasSmllet) {
+            cout << "Error: Password must contain at least 2 letters, including at least 1 uppercase and 1 lowercase letter." << endl;
+        } else {
+            break;
+        }
+    }
+
+    cout << "Enter initial balance: $";
+    cin >> newUser.balance;
+
+    newUser.id = static_cast<int>(database.size()) + 1;
+    newUser.registerDate = "2023-11-27";
+
+    database.push_back(newUser);
+
+    cout << "Registration successful. Your ID: " << newUser.id << endl;
+}
+
+int main() {
+    while (true) {
+        cout << "Welcome to the bank system!" << endl;
+        cout << "1. Login" << endl;
+        cout << "2. Register" << endl;
+        cout << "3. Exit" << endl;
+        cout << "Choose an option: ";
+
+        int choice;
+        cin >> choice;
+
+        switch (choice) {
+            case 1: {
+                string login, password;
+                cout << "Enter login: ";
+                cin >> login;
+
+                cout << "Enter password: ";
+                cin >> password;
+
+                if (isUserExists(login, password)) {
+                    int userIndex = getUserIndex(login, password);
+                    User& currentUser = database[userIndex];
+
+                    while (true) {
+                        cout << "1. Show Balance" << endl;
+                        cout << "2. Withdraw Cash" << endl;
+                        cout << "3. Deposit Cash" << endl;
+                        cout << "4. Logout" << endl;
+                        cout << "Choose an option: ";
+
+                        int userChoice;
+                        cin >> userChoice;
+
+                        switch (userChoice) {
+                            case 1:
+                                showBalance(currentUser);
+                            case 2: {
+                                double amount;
+                                cout << "Enter amount to withdraw: $";
+                                cin >> amount;
+                                withdrawCash(currentUser, amount);
+                            }
+                            case 3: {
+                                double amount;
+                                cout << "Enter amount to deposit: $";
+                                cin >> amount;
+                                depositCash(currentUser, amount);
+                                break;
+                            }
+                            case 4:
+                                cout << "Logging out..." << endl;
+                                break;
+                            default:
+                                cout << "Invalid option. Try again." << endl;
+                        }
+
+                        if (userChoice == 4) {
+                            break;
+                        }
+                    }
+                } else {
+                    cout << "Invalid login or password. Please try again." << endl;
+                }
+                break;
+            }
+            case 2:
+                registerUser();
+                break;
+            case 3:
+                cout << "Exiting the bank system. Goodbye!" << endl;
+                return 0;
+            default:
+                cout << "Invalid option. Try again." << endl;
+        }
+    }
+
+    return 0;
+}
